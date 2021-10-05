667644cd110b4b058ce0390d55217155e83508be
iscodeminister <50560278+iscodeminister@users.noreply.github.com>
2021-10-05 09:24:58 +0800

    Update 拋體運動一hw2csv.py

--- 第一堂課-拋體運動/拋體運動一hw2csv.py
+++ 第一堂課-拋體運動/拋體運動一hw2csv.py
@@ -9,7 +9,9 @@ L=15 #箱子長度
 bangles=[15,30,45,60,75]
 scene = canvas(title="拋體運動", width=800, height=400, x=0, y=0)
 floor = box(pos=vec(0,0,0),length=L, height=0.02, width=10)
-#man = box(pos=(),length=L, height=0.02, width=1)
+bx=-floor.length/2
+by=floor.height/2
+man = box(pos=(bx,by,0),length=0.5, height=1.8, width=1)
 
 csvFile = open('out3.csv','w',newline='') #csv
 Writer = csv.writer(csvFile)
