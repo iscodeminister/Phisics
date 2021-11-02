0eee8f2662e386d99b83d9a9a3d07e4fea5646da
iscodeminister <50560278+iscodeminister@users.noreply.github.com>
2021-11-02 10:44:33 +0800

    A bunch of update

--- 第一堂課-拋體運動四/拋體運動三csvN.py
+++ 第一堂課-拋體運動四/拋體運動三csvN.py
@@ -3,20 +3,20 @@ import csv
 import math
 
 """
-設計場景排球
+設計場景
 """
-L=18 #箱子長度
+L=30 #箱子長度
 scene = canvas(title="拋體運動三", width=1600, height=900, x=0, y=0)
-floor = box(pos=vec(0, 0, 0),length=L, height=0.01, width=9,opacity=0.5)
+floor = box(pos=vec(0, 0, 0),length=L, height=0.01, width=8,opacity=0.5)
 end = -L/2
 arrow(axis=vector(1,0,0),color=vector(1, 0, 0),shaftwidth=0.02,opacity=0.4,pos=vec(end,0,0))
 arrow(axis=vector(0,1,0),color=vector(0, 1, 0),shaftwidth=0.02,opacity=0.4,pos=vec(end,0,0))
 arrow(axis=vector(0,0,1),color=vector(0, 0, 1),shaftwidth=0.02,opacity=0.4,pos=vec(end,0,0))
-mass = 0.280
-v0 = 15
+mass = 0.280 #weight
+v0 = 10 #start speed
 radius = 0.105
 bangle=45
-the= radians(bangle)
+theta= radians(bangle)
 g = vector(0,-9.8,0)
 
 csvFile = open('out.csv','w',newline='') #csv
@@ -27,16 +27,21 @@ Writer.writerow(['Ball','Distance'])
 def
 """
 bally = radius + floor.height/2
-ball_a = sphere(radius=radius,make_trail=True,pos=vector(end,bally,0),interval=10,color=color.green,opacity=0.4) #不考慮空氣阻力的透明球
-ball_b = sphere(radius=radius,make_trail=True,pos=vector(end,bally,0),interval=10,color=color.yellow) #考慮空氣阻力的球
-scene.camera.follow(ball_b)
+ball_a = sphere(radius=radius,make_trail=True,
+                pos=vector(end,bally,0),
+                interval=10,color=color.green,
+                opacity=0.4) #不考慮空氣阻力的透明球
+ball_b = sphere(radius=radius,make_trail=True,
+                pos=vector(end,bally,0),
+                interval=10,color=color.yellow) #考慮空氣阻力的球
+scene.camera.follow(ball_b) #Camera follow ball b
 #ball a
 #ball_a.pos.y = ball_a.radius+floor.height/2
-ball_a.theta = the
+ball_a.theta = theta
 ball_a.y0 = ball_a.pos.y
 ball_a.m = mass
 ball_a.v0 = v0
-ball_a.v = vector(          
+ball_a.v = vector(
 	ball_a.v0*cos(ball_a.theta),
 	ball_a.v0*sin(ball_a.theta),
 	0
@@ -45,7 +50,7 @@ ball_a.a = g
 
 #ball b
 #ball_b.pos.y = ball_b.radius+floor.height/2
-ball_b.theta = the
+ball_b.theta = theta
 ball_b.y0 = ball_b.pos.y
 ball_b.m = mass
 ball_b.v0 = v0
@@ -54,15 +59,18 @@ ball_b.v = vector(
 	ball_b.v0*sin(ball_b.theta),
 	0
 )
-ball_b.a = g 
-
+ball_b.a = g
 """
 計算迴圈
 """
+sqrt=math.sqrt(1)
+#eta_air = 1.81e-5
+#b = 6*pi*eta_air*ball_b.radius	
+b=1.225*-0.5*v0*v0*0.5*pi*radius*radius*sqrt
 dt = 0.0001
 while True:
-	rate(2000)
-	if ball_a.pos.y < ball_a.y0 and ball_b.pos.y < ball_b.y0:
+	rate(4000)
+	if ball_a.pos.y >= ball_a.y0 and ball_b.pos.y < ball_b.y0:
 		break
 
 	if ball_a.pos.y >= ball_a.y0:
@@ -70,12 +78,11 @@ while True:
 		ball_a.v += ball_a.a*dt
 	if ball_b.pos.y >= ball_b.y0:
 		ball_b.pos += ball_b.v*dt
-		ball_b.a = g + (1.2041*-0.5*ball_b.v.mag2*0.5*pi*ball_b.radius**2*ball_b.v.norm())/ball_b.m
+		ball_b.a = g + (-b*ball_b.v/ball_b.m)
 		ball_b.v += ball_b.a*dt
-
 """
 counting section
-
+"""
 ball_a_label = label(pos=ball_a.pos,
     text=ball_a.pos.x-end, xoffset=20,
     yoffset=12, space=ball_a.radius,
@@ -86,10 +93,9 @@ ball_b_label = label(pos=ball_b.pos,
     yoffset=25, space=ball_b.radius,
     height=8, border=1,
     font='sans')
-"""
+
 print("球A飛了",ball_a.pos.x-end,"公尺",sep='')
 print("球B飛了",ball_b.pos.x-end,"公尺",sep='')
-print(ball_b.v.norm())
 Writer.writerow(["A",ball_a.pos.x])
 Writer.writerow(["B",ball_b.pos.x])
 
